2b7dbb696ef7fbf008d0462cc806e02853c98524
arturombc <arturomagno1@gmail.com>
2025-05-20 15:26:14 -0500

    modify the ipynb, new files

--- analysis2/analysis.R
+++ analysis2/analysis.R
@@ -1,5 +1,5 @@
 library(renv)
-renv::load("~/Study/UTEC/2025-1/adv_bioinf")
+renv::load("~/Study/UTEC/2025-1/adv_bioinf") # se usa para cargar librerías, no es necesario si se instala seqinr
 library(seqinr)
 
 aaseq<-read.fasta("aa17_explore.fasta" ,seqtype = "AA")
