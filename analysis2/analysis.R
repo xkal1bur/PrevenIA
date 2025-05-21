2c9358f18eee9c62a0a60842816ab590a99cf070
arturombc <arturomagno1@gmail.com>
2025-05-21 06:49:13 -0500

    new graph

--- analysis2/analysis.R
+++ analysis2/analysis.R
@@ -2,6 +2,9 @@ library(renv)
 renv::load("~/Study/UTEC/2025-1/adv_bioinf") # se usa para cargar librerías, no es necesario si se instala seqinr
 library(seqinr)
 
-aaseq<-read.fasta("aa17_explore.fasta" ,seqtype = "AA")
-aaseq<-aaseq[[1]]
+aaseq<-read.fasta("aa17_explore.fasta" ,seqtype = "AA")[[1]]
 AAstat(aaseq)
+
+
+aaseq2<-read.fasta("aa13_explore.fasta" ,seqtype = "AA")[[1]]
+AAstat(aaseq2)
