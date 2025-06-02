a619a154d89c799f25d690c345a93aabb99b9e0e
LeonardoHuaman <leonardo.huaman.c@utec.edu.pe>
2025-06-02 16:50:25 -0500

    Pacientes, Doctor, Login, Register, BD

---
+++ backend/app/schemas.py
@@ -0,0 +1,65 @@
+from datetime import datetime
+from pydantic import BaseModel, EmailStr
+from typing import Optional
+
+class DoctorCreate(BaseModel):
+    nombre: str
+    especialidad: str
+    correo: EmailStr
+    password: str
+    clinic_name: str
+
+# Esquema para serializar la respuesta después de crear un doctor:
+class DoctorOut(BaseModel):
+    id: int
+    nombre: str
+    especialidad: str
+    correo: EmailStr
+    creado_el: datetime
+    clinic_name: str
+
+    class Config:
+        from_attributes = True  
+
+# Esquema para el token:
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    correo: EmailStr | None = None
+
+class DoctorLogin(BaseModel):
+    correo: EmailStr
+    password: str
+
+class DoctorProfile(BaseModel):
+    id: int
+    nombre: str
+    clinic_name: str
+
+    class Config:
+        from_attributes = True
+
+class PacienteCreate(BaseModel):
+    dni: str
+    nombres: str
+    apellidos: str
+    edad: int
+    celular: str
+    correo: EmailStr
+    doctor_id: int     
+
+class PacienteOut(BaseModel):
+    id: int
+    dni: str
+    nombres: str
+    apellidos: str
+    edad: int
+    celular: str
+    correo: EmailStr
+    foto: Optional[str]
+    doctor_id: int
+
+    class Config:
+        from_attributes = True
